8125c863f2dc9341433401fe6f0c83ab01f3e0bf
Caio Vieira <caiodmvieiragithub@gmail.com>
2020-05-02 12:49:47 -0300

    Styling Details

--- src/pages/Dashboard/index.tsx
+++ src/pages/Dashboard/index.tsx
@@ -1,5 +1,6 @@
 import React, { useState, useEffect, FormEvent } from 'react';
-import { FiChevronRight } from 'react-icons/fi'
+import { FiChevronRight } from 'react-icons/fi';
+import { Link } from 'react-router-dom';
 
 import api from '../../services/api';
 import logoImg from '../../assets/logo.svg';
@@ -71,17 +72,17 @@ const Dashboard: React.FC = () => {
 
       <Repositories>
         { repositories.map(repository => (
-          <a key={repository.full_name} href="teste">
-          <img
-            src={repository.owner.avatar_url}
-            alt={repository.owner.login}/>
-          <div>
-            <strong>{repository.full_name}</strong>
-            <p>{repository.description}</p>
-          </div>
+          <Link key={repository.full_name} to={`/repositories/${repository.full_name}`}>
+            <img
+              src={repository.owner.avatar_url}
+              alt={repository.owner.login}/>
+            <div>
+              <strong>{repository.full_name}</strong>
+              <p>{repository.description}</p>
+            </div>
 
-          <FiChevronRight size={20} />
-        </a>
+            <FiChevronRight size={20} />
+          </Link>
         ))}
       </Repositories>
     </>
--- src/pages/Repository/index.tsx
+++ src/pages/Repository/index.tsx
@@ -1,7 +1,65 @@
 import React from 'react';
+import { useRouteMatch, Link } from 'react-router-dom';
+import { FiChevronLeft, FiChevronRight } from 'react-icons/fi';
+
+import { Header,RepositoryInfo, Issues } from './styles';
+import logoImg from '../../assets/logo.svg';
+
+interface RepositoryParams {
+  repository: string;
+};
+
 
 const Repository: React.FC = () => {
-  return <h1>Repository</h1>;
+  const { params } = useRouteMatch<RepositoryParams>();
+
+  return (
+    <>
+      <Header>
+        <img src={logoImg} alt="Githug Explorer" />
+        <Link to="/">
+          <FiChevronLeft size={16} />
+          Voltar
+        </Link>
+      </Header>
+
+      <RepositoryInfo>
+        <header>
+          <img src="" alt="RocketSeat"></img>
+          <div>
+            <strong>rocketseat/unform</strong>
+            <p>descricao</p>
+          </div>
+        </header>
+        <ul>
+          <li>
+            <strong>1008</strong>
+            <span>Stars</span>
+          </li>
+          <li>
+            <strong>48</strong>
+            <span>Forks</span>
+          </li>
+          <li>
+            <strong>67</strong>
+            <span>Issues Abertas</span>
+          </li>
+        </ul>
+      </RepositoryInfo>
+
+      <Issues>
+        <Link  to={"shsh"}>
+
+            <div>
+              <strong>sjdauj</strong>
+              <p>suhduohs</p>
+            </div>
+
+            <FiChevronRight size={20} />
+          </Link>
+      </Issues>
+    </>
+  );
 };
 
 export default Repository;
